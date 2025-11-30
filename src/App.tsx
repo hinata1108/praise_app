e2d3e5d0cbd3eb52b46a981178897703b72855cf
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2025-11-30 23:18:43 +0900

    Homeコンポーネントを削除し、Postコンポーネントを追加。ルーティングを更新。

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // react-router-domのインポートを追加
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
-import { Home } from "./pages/home";
+import { Post } from "./pages/post";
 import { Login } from "./pages/login";
 import { Mypost } from "./pages/mypost";
 import { Posts } from "./pages/posts";
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Link to="/">Home</Link>
+        <Link to="/post">Post</Link>
         <br />
         <Link to="/login">Login</Link>
         <br />
@@ -20,8 +20,8 @@ function App() {
         <br />
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/post" element={<Post />} />
           <Route path="/mypost" element={<Mypost />} />
           <Route path="/posts" element={<Posts />} />
         </Routes>
